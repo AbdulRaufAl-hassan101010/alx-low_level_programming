b69505c7d02bf3298b254d86326469800072fec2
AbdulRaufAl-hassan101010 <abdulraufalhassan101010@gmail.com>
2022-09-09 15:09:56 -0700

    wrote a function that tell weather a number is a positive or negative integer

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdio>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *main - main block
+ *Description: get a random number
+ *and display if number is positive or negative
+ *Return: 0 always
+ */
+int main(void)
+{
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+
+/* your code goes there */
+if (n >= 0)
+{
+printf("%i is positive\n", n);
+}
+else
+{
+printf("%i is negative\n", n);
+}
+return (0);
+}

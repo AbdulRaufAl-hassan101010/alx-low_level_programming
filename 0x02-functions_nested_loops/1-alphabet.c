2fb2ce9207f28bf8ccd0eef5e49ca477131bd878
AbdulRaufAl-hassan101010 <abdulraufalhassan101010@gmail.com>
2022-09-13 12:01:49 -0700

    second commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabets
+ *
+ * Return: Always 0.
+ */
+
+int print_alphabet(void)
+{
+	char alphabet = "a";
+
+	while (alphabet = < "z")
+	{
+		_putchar(alphabet);
+		aphabet++;
+	}
+
+	return (0);
+}

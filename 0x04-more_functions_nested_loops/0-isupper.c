516dc916175cb926def4792b59ea19bee00398f5
AbdulRaufAl-hassan101010 <abdulraufalhassan101010@gmail.com>
2022-09-15 12:27:07 -0700

    q0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+/**
+ * isupper - check for uppercase
+ * @c: takes character as input
+ * Return: 1 for  if char is a uppercase
+ * 0 for lowercase
+ */
+
+int _isupper(int c)
+{
+if (c < 65 && c > 90)
+{
+	return (0);
+}
+
+	return (1);
+}

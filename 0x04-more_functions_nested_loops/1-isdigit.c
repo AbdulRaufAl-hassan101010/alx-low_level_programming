560c3667b8a2cc555f070dce0962e85b0d60e61f
AbdulRaufAl-hassan101010 <abdulraufalhassan101010@gmail.com>
2022-09-15 12:48:09 -0700

    push

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,15 +1,15 @@
 #include "main.h"i;
 
 /**
- * _isupper - check for uppercase
- * @c: takes character as input
+ * _isdigit - check for uppercase
+ * @c: expecting  didgit as input
  * Return: 1 for  if char is a uppercase
  * 0 for lowercase
  */
 
 int _isupper(int c)
 {
-if (c < 65 || c > 90)
+if (c < 48 || c > 57)
 {i
 	return (0);
 }
